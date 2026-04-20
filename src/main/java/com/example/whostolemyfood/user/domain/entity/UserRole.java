40b5284bd3bf21ae38bfa810404fb165801f0d4f
musoyou12 <charlie123000@gmail.com>
2026-04-20 16:22:03 +0900

    feat: 유저 엔티티,UserRole, 레포지토리 구현

--- src/main/java/com/example/whostolemyfood/user/domain/entity/UserRole.java
+++ src/main/java/com/example/whostolemyfood/user/domain/entity/UserRole.java
@@ -1,4 +1,15 @@
 package com.example.whostolemyfood.user.domain.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
+    CUSTOMER("고객"),
+    OWNER("가게 주인"),
+    MANAGER("서비스 담당자"),
+    MASTER("최종 관리자");
+
+    private final String description;
 }
