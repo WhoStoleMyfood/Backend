b636bda17b000591d29981dc14278593ad7b9958
musoyou12 <charlie123000@gmail.com>
2026-04-22 13:04:19 +0900

    feat: 에러처리

--- src/main/java/com/example/whostolemyfood/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/whostolemyfood/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package com.example.whostolemyfood.global.exception;
 
-import org.springframework.http.HttpStatus;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
