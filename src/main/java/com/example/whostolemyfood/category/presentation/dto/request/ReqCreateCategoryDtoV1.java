c8c54917f645cb39e4a188d8443adb5b39d91999
Jieunbakk <jieun134@knu.ac.kr>
2026-04-22 13:16:58 +0900

    feat: ReqCreateCategoryDto

---
+++ src/main/java/com/example/whostolemyfood/category/presentation/dto/request/ReqCreateCategoryDtoV1.java
@@ -0,0 +1,14 @@
+package com.example.whostolemyfood.category.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqCreateCategoryDtoV1 {
+    @NotBlank(message = "카테고리명은 필수입니다.")
+    private String name;
+}
