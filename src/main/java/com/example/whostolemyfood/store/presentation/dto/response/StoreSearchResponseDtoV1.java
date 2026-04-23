18dd1e113764a4726b49f0649f66b7515ee520b8
Jieunbakk <jieun134@knu.ac.kr>
2026-04-23 11:48:13 +0900

    feat: CustomRepository 구현

--- src/main/java/com/example/whostolemyfood/store/presentation/dto/response/StoreSearchResponseDtoV1.java
+++ src/main/java/com/example/whostolemyfood/store/presentation/dto/response/StoreSearchResponseDtoV1.java
@@ -1,9 +1,48 @@
 package com.example.whostolemyfood.store.presentation.dto.response;
 
+import com.example.whostolemyfood.store.domain.entity.StoreStatus;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
 public class StoreSearchResponseDtoV1 {
+    private UUID storeId;
+    private String storeName;
+    private String storeAddress;
+    private String storePhone;
+    private String content;
+    private Integer minOrderPrice;
+    private StoreStatus status;
+    private LocalTime openTime;
+    private LocalTime closeTime;
+
+    @QueryProjection
+    @Builder
+    public StoreSearchResponseDtoV1(
+            UUID storeId,
+            String storeName,
+            String storeAddress,
+            String storePhone,
+            String content,
+            Integer minOrderPrice,
+            StoreStatus status,
+            LocalTime openTime,
+            LocalTime closeTime
+    ) {
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.storePhone = storePhone;
+        this.content = content;
+        this.minOrderPrice = minOrderPrice;
+        this.status = status;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
 }
